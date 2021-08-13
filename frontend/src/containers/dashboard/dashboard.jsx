dc50218b2c501a4932060d49fbe3cd87ae3e70d1
Mihir Karbelkar <mihir@madstreetden.com>
2021-08-13 23:06:34 +0530

    added mf and crypto

--- frontend/src/containers/dashboard/dashboard.jsx
+++ frontend/src/containers/dashboard/dashboard.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Tabs, Tab } from "react-bootstrap";
-import { Card } from 'components';
-import { dashboardStocks, newsList, stockSummary } from 'atoms/dashboard';
-import { useRecoilValue } from 'recoil';
-import StockList from './stockList';
-import NewsList from './newsList';
-import Summary from './summary';
+import { Card } from "components";
+import { dashboardStocks, newsList, stockSummary } from "atoms/dashboard";
+import { useRecoilValue } from "recoil";
+import StockList from "./stockList";
+import NewsList from "./newsList";
+import Summary from "./summary";
 
 import "./style.scss";
 
@@ -13,6 +13,66 @@ const AddedTag = () => {
   return <div className="added-tag secondary-text-color">Added</div>;
 };
 
+const InvestmentBlock = (props) => {
+  const { dashboardOverview, latestNews, investmentSummary, type } = props;
+  return (
+    <div className="d-flex explore-investment-wrapper flex-column">
+      <div className="d-flex first-row">
+        <Card title="Top 5 in Market" className="top-market">
+          <StockList
+            list={dashboardOverview}
+            cols={["label", "image", "price", "chart"]}
+            onAddWatchlist={() => null}
+            type={`${type}`}
+          />
+        </Card>
+        <Card className="all-investments">
+          <Tabs defaultActiveKey="self" className="mb-3">
+            <Tab eventKey="self" title="Your Investments">
+              <div className="d-flex flex-column">
+                <Summary summary={investmentSummary} />
+                <StockList
+                  list={investmentSummary?.companies}
+                  cols={["label", "price"]}
+                  type={`${type}`}
+                />
+              </div>
+            </Tab>
+            <Tab eventKey="ai" title="AI Investments">
+              <div className="d-flex flex-column">
+                <Summary summary={investmentSummary} />
+                <StockList
+                  list={investmentSummary?.companies}
+                  cols={["label", "price"]}
+                  type={`${type}`}
+                />
+              </div>
+            </Tab>
+          </Tabs>
+        </Card>
+      </div>
+      <div className="d-flex second-row">
+        <Card title="Top Gainers" className="top-gainers">
+          <StockList
+            list={dashboardOverview}
+            cols={["label", "price"]}
+            type={`${type}`}
+          />
+        </Card>
+        <Card title="Top Losers" className="top-losers">
+          <StockList
+            list={dashboardOverview}
+            cols={["label", "price"]}
+            type={`${type}`}
+          />
+        </Card>
+        <Card title={`Latest News on ${props.tab}`} className="latest-news">
+          <NewsList list={latestNews} />
+        </Card>
+      </div>
+    </div>
+  );
+};
 const Dashboard = () => {
   const dashboardOverview = useRecoilValue(dashboardStocks);
   const latestNews = useRecoilValue(newsList);
@@ -21,46 +81,31 @@ const Dashboard = () => {
   return (
     <Tabs defaultActiveKey="stocks" className="mb-3">
       <Tab eventKey="stocks" title="Stocks">
-        <div className="d-flex explore-investment-wrapper flex-column">
-          <div className="d-flex first-row">
-            <Card title="Top 5 in Market" className="top-market">
-              <StockList list={dashboardOverview} cols={['label', 'image', 'price', 'chart']} onAddWatchlist={() => null} type="stock" />
-            </Card>
-            <Card className="all-investments">
-              <Tabs defaultActiveKey="self" className="mb-3">
-                <Tab eventKey="self" title="Your Investments">
-                  <div className="d-flex flex-column">
-                    <Summary summary={investmentSummary} />
-                    <StockList list={investmentSummary?.companies} cols={['label', 'price']} type="stock" />
-                  </div>
-                </Tab>
-                <Tab eventKey="ai" title="AI Investments">
-                  <div className="d-flex flex-column">
-                    <Summary summary={investmentSummary} />
-                    <StockList list={investmentSummary?.companies} cols={['label', 'price']} type="stock" />
-                  </div>
-                </Tab>
-              </Tabs>
-            </Card>
-          </div>
-          <div className="d-flex second-row">
-            <Card title="Top Gainers" className="top-gainers">
-              <StockList list={dashboardOverview} cols={['label', 'price']} type="stock" />
-            </Card>
-            <Card title="Top Losers" className="top-losers">
-              <StockList list={dashboardOverview} cols={['label', 'price']} type="stock" />
-            </Card>
-            <Card title="Latest New on Stocks" className="latest-news">
-              <NewsList list={latestNews} />
-            </Card>
-          </div>
-        </div>
+        <InvestmentBlock
+          type="stocks"
+          dashboardOverview={dashboardOverview["stocks"]}
+          latestNews={latestNews["stocks"]}
+          investmentSummary={investmentSummary["stocks"]}
+          tab={"Stocks"}
+        />
       </Tab>
       <Tab eventKey="mf" title="Mutual Funds">
-        
+        <InvestmentBlock
+          type="mf"
+          dashboardOverview={dashboardOverview["mf"]}
+          latestNews={latestNews["mf"]}
+          investmentSummary={investmentSummary["mf"]}
+          tab={"Mutual Funds"}
+        />
       </Tab>
       <Tab eventKey="crypto" title="Crypto">
-        
+        <InvestmentBlock
+          type="crypto"
+          dashboardOverview={dashboardOverview["crypto"]}
+          latestNews={latestNews["crypto"]}
+          investmentSummary={investmentSummary["crypto"]}
+          tab={"Crypto"}
+        />
       </Tab>
     </Tabs>
   );
--- frontend/src/recoil/atoms/dashboard.js
+++ frontend/src/recoil/atoms/dashboard.js
@@ -1,91 +1,205 @@
-import { atom } from 'recoil';
+import { atom } from "recoil";
 
 export const dashboardStocks = atom({
-  key: 'dashboardStocks',
-  default: [{
-    image: 'https://picsum.photos/200',
-    label: 'Reliance Industries',
-    symbol: 'RELIANCE',
-    currentPrice: 2013,
-    previousPrice: 2000,
-    chartData: []
-  }, {
-    image: 'https://picsum.photos/200',
-    label: 'Reliance Industries',
-    symbol: 'RELIANCE',
-    currentPrice: 2013,
-    previousPrice: 2000,
-    chartData: []
-  }, {
-    image: 'https://picsum.photos/200',
-    label: 'Reliance Industries',
-    symbol: 'RELIANCE',
-    currentPrice: 2013,
-    previousPrice: 2000,
-    chartData: []
-  }, {
-    image: 'https://picsum.photos/200',
-    label: 'Reliance Industries',
-    symbol: 'RELIANCE',
-    currentPrice: 2013,
-    previousPrice: 2000,
-    chartData: []
-  }, {
-    image: 'https://picsum.photos/200',
-    label: 'Reliance Industries',
-    symbol: 'RELIANCE',
-    currentPrice: 2013,
-    previousPrice: 2000,
-    chartData: []
-  }]
+  key: "dashboardStocks",
+  default: {
+    stocks: [
+      {
+        image: "https://picsum.photos/200",
+        label: "Reliance Industries",
+        symbol: "RELIANCE",
+        currentPrice: 2013,
+        previousPrice: 2000,
+        chartData: [],
+      },
+      {
+        image: "https://picsum.photos/200",
+        label: "Reliance Industries",
+        symbol: "RELIANCE",
+        currentPrice: 2013,
+        previousPrice: 2000,
+        chartData: [],
+      },
+      {
+        image: "https://picsum.photos/200",
+        label: "Reliance Industries",
+        symbol: "RELIANCE",
+        currentPrice: 2013,
+        previousPrice: 2000,
+        chartData: [],
+      },
+      {
+        image: "https://picsum.photos/200",
+        label: "Reliance Industries",
+        symbol: "RELIANCE",
+        currentPrice: 2013,
+        previousPrice: 2000,
+        chartData: [],
+      },
+      {
+        image: "https://picsum.photos/200",
+        label: "Reliance Industries",
+        symbol: "RELIANCE",
+        currentPrice: 2013,
+        previousPrice: 2000,
+        chartData: [],
+      },
+    ],
+    crypto: [
+      {
+        image: "https://picsum.photos/200",
+        label: "Reliance Industries",
+        symbol: "RELIANCE",
+        currentPrice: 2013,
+        previousPrice: 2000,
+        chartData: [],
+      },
+      {
+        image: "https://picsum.photos/200",
+        label: "Reliance Industries",
+        symbol: "RELIANCE",
+        currentPrice: 2013,
+        previousPrice: 2000,
+        chartData: [],
+      },
+    ],
+    mf: [
+      {
+        image: "https://picsum.photos/200",
+        label: "Reliance Industries",
+        symbol: "RELIANCE",
+        currentPrice: 2013,
+        previousPrice: 2000,
+        chartData: [],
+      },
+      {
+        image: "https://picsum.photos/200",
+        label: "Reliance Industries",
+        symbol: "RELIANCE",
+        currentPrice: 2013,
+        previousPrice: 2000,
+        chartData: [],
+      },
+    ],
+  },
 });
 
 export const newsList = atom({
-  key: 'latestNews',
-  default: [{
-    image: 'https://picsum.photos/200',
-    excerpt: 'Lorem ipsum dolor sit amet'
-  }, {
-    image: 'https://picsum.photos/200',
-    excerpt: 'Lorem ipsum dolor sit amet'
-  }, {
-    image: 'https://picsum.photos/200',
-    excerpt: 'Lorem ipsum dolor sit amet'
-  }, {
-    image: 'https://picsum.photos/200',
-    excerpt: 'Lorem ipsum dolor sit amet'
-  }, {
-    image: 'https://picsum.photos/200',
-    excerpt: 'Lorem ipsum dolor sit amet'
-  }]
+  key: "latestNews",
+  default: {
+    stocks: [
+      {
+        image: "https://picsum.photos/200",
+        excerpt: "Lorem ipsum dolor sit amet",
+      },
+      {
+        image: "https://picsum.photos/200",
+        excerpt: "Lorem ipsum dolor sit amet",
+      },
+      {
+        image: "https://picsum.photos/200",
+        excerpt: "Lorem ipsum dolor sit amet",
+      },
+      {
+        image: "https://picsum.photos/200",
+        excerpt: "Lorem ipsum dolor sit amet",
+      },
+      {
+        image: "https://picsum.photos/200",
+        excerpt: "Lorem ipsum dolor sit amet",
+      },
+    ],
+    mf: [
+      {
+        image: "https://picsum.photos/200",
+        excerpt: "Lorem ipsum dolor sit amet",
+      },
+      {
+        image: "https://picsum.photos/200",
+        excerpt: "Lorem ipsum dolor sit amet",
+      },
+    ],
+    crypto: [
+      {
+        image: "https://picsum.photos/200",
+        excerpt: "Lorem ipsum dolor sit amet",
+      },
+      {
+        image: "https://picsum.photos/200",
+        excerpt: "Lorem ipsum dolor sit amet",
+      },
+    ],
+  },
 });
 
 export const stockSummary = atom({
-  key: 'stockSummary',
+  key: "stockSummary",
   default: {
-    invested: 100000,
-    returns: 124000,
-    companies: [{
-      image: 'https://picsum.photos/200',
-      label: 'Reliance Industries',
-      symbol: 'RELIANCE',
-      currentPrice: 2013,
-      previousPrice: 2000,
-      chartData: []
-    }, {
-      image: 'https://picsum.photos/200',
-      label: 'Reliance Industries',
-      symbol: 'RELIANCE',
-      currentPrice: 2013,
-      previousPrice: 2000,
-      chartData: []
-    }, {
-      image: 'https://picsum.photos/200',
-      label: 'Reliance Industries',
-      symbol: 'RELIANCE',
-      currentPrice: 2013,
-      previousPrice: 2000,
-      chartData: []
-    }]
-  }
+    stocks: {
+      invested: 100000,
+      returns: 124000,
+      companies: [
+        {
+          image: "https://picsum.photos/200",
+          label: "Reliance Industries",
+          symbol: "RELIANCE",
+          currentPrice: 2013,
+          previousPrice: 2000,
+          chartData: [],
+        },
+        {
+          image: "https://picsum.photos/200",
+          label: "Reliance Industries",
+          symbol: "RELIANCE",
+          currentPrice: 2013,
+          previousPrice: 2000,
+          chartData: [],
+        },
+        {
+          image: "https://picsum.photos/200",
+          label: "Reliance Industries",
+          symbol: "RELIANCE",
+          currentPrice: 2013,
+          previousPrice: 2000,
+          chartData: [],
+        },
+      ],
+    },
+    crypto: {
+      invested: 15600,
+      returns: 120900,
+      companies: [
+        {
+          image: "https://picsum.photos/200",
+          label: "Reliance Industries",
+          symbol: "RELIANCE",
+          currentPrice: 2013,
+          previousPrice: 2000,
+          chartData: [],
+        },
+      ],
+    },
+    mf: {
+      invested: 123412,
+      returns: 24432,
+      companies: [
+        {
+          image: "https://picsum.photos/200",
+          label: "Reliance Industries",
+          symbol: "RELIANCE",
+          currentPrice: 2013,
+          previousPrice: 2000,
+          chartData: [],
+        },
+        {
+          image: "https://picsum.photos/200",
+          label: "Reliance Industries",
+          symbol: "RELIANCE",
+          currentPrice: 2013,
+          previousPrice: 2000,
+          chartData: [],
+        },
+      ],
+    },
+  },
 });
